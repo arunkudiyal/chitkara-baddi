95dcd38299913c7ee04206f944e54eb3dd93dad4
Arun Kudiyal <37402172+arunkudiyal@users.noreply.github.com>
2021-01-16 09:26:52 +0530

    Updated COde

--- Code/StackUsingLL.java
+++ Code/StackUsingLL.java
@@ -42,10 +42,45 @@ public class StackUsingLL {
     }
 
     public static void pop() {
-
+        if(top == null) {
+            System.out.println("STACK UNDERFLOW");
+            System.exit(1);
+        }
+        System.out.println(top.data + " has been popped successfully!");
+        top = top.next;
     }
+    
+    public static void display() {
+        if(top == null) {
+            System.out.println("STACK UNDERFLOW!");
+            System.exit(1);
+        } else {
+                Node temp = top;
+                while(temp != null) {
+                System.out.println(temp.data + " ");
+                System.out.println();
+            }
+        }    
+    }    
 
     public static void main(String[] args) {
-
+        StackUsingLL stack = new StackUsingLL();
+        stack.push(100);
+        stack.push(200);
+        stack.push(300);
+        stack.push(400);
+        stack.push(500);
+        
+        stack.display();
+        stack.pop();
+        stack.pop();
+        
+        System.out.println("The top-most element in the stack is: " + stack.peek());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        
+        stack.display();
+        stack.pop();
     }
 }
